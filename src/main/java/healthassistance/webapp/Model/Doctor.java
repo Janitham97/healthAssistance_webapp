ac21e8ceadb3260ed5172c09fe86774d89db30d3
Janitha Missaka <janitha.missaka@gmail.com>
2020-11-02 09:29:25 +0530

    first commit

---
+++ src/main/java/healthassistance/webapp/Model/Doctor.java
@@ -0,0 +1,21 @@
+package healthassistance.webapp.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@ToString
+
+@Document(collection = "users")
+public class Doctor {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String NIC;
+    private String location;
+}
---
+++ src/main/java/healthassistance/webapp/Repository/PatientRepository.java
@@ -0,0 +1,7 @@
+package healthassistance.webapp.Repository;
+
+import healthassistance.webapp.Model.Patient;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface PatientRepository extends MongoRepository<Patient,String> {
+}
