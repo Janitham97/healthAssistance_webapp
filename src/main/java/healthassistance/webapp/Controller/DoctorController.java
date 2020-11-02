35719013031aba3d5a92510d316387335a9dcc55
Janitha Missaka <janitha.missaka@gmail.com>
2020-11-02 15:47:38 +0530

    Update Patient Restcontroller

---
+++ src/main/java/healthassistance/webapp/Controller/DoctorController.java
@@ -0,0 +1,4 @@
+package healthassistance.webapp.Controller;
+
+public class DoctorController {
+}
---
+++ src/main/java/healthassistance/webapp/Controller/PatientController.java
@@ -0,0 +1,42 @@
+package healthassistance.webapp.Controller;
+
+import healthassistance.webapp.Model.Patient;
+import healthassistance.webapp.Repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class PatientController {
+
+    @Autowired
+    private PatientRepository patientRepository;  //inject repository
+
+    //add save method
+    @PostMapping("/addPatient")
+    public String savePatient(@RequestBody Patient patient){
+        patientRepository.save(patient);
+        return "Add a Patient with id"+ patient.getId();
+    }
+
+    //get all patients method
+    @GetMapping("/findAllPatients")
+    public List<Patient> getPatient(){
+        return patientRepository.findAll();
+    }
+
+    //get patient by id
+    @GetMapping("/findPatientByid/{}id")
+    public Optional<Patient> getPatient(@PathVariable String id){
+        return patientRepository.findById(id);
+    }
+
+    //delete patient
+    @DeleteMapping("/delete/{id}")
+    public String deletePatient(@PathVariable String id){
+        patientRepository.deleteById(id);
+        return "Patient deleted with id";
+    }
+}
