cf47f96855c00ab5e2dbf70a95ed590dedaf4aa6
Janitha Missaka <janitha.missaka@gmail.com>
2020-11-16 12:11:26 +0530

    add login form and route

--- src/main/FrontEnd/reactjs/src/Components/LoginForum.js
+++ src/main/FrontEnd/reactjs/src/Components/LoginForum.js
@@ -1,36 +1,53 @@
 import React, {Component} from "react";
-import {Form, Button, Container, Card} from "react-bootstrap";
+import {Form, Button, Container, Card, Row, Col} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 class LoginForum extends Component{
     render() {
+
         return(
             <div>
                 <br/>
+                <br/>
+                <br/>
+                <br/>
                 <Container>
-               <Card style={{ width: '18rem' }}>
-                   <Card.Body>
-                <Form>
-                    <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                            We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+                    <Row>
+                        <Col></Col>
+                        <Col><Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <Form>
+                                    <Form.Group controlId="formBasicEmail">
+                                        <Form.Label>NIC</Form.Label>
+                                        <Form.Control type="email" placeholder="Enter Your NIC" />
+                                        <Form.Text className="text-muted">
+                                            We'll never share your data with anyone else.
+                                        </Form.Text>
+                                    </Form.Group>
 
-                    <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                    <Form.Group controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-                   </Card.Body>
-                </Card>
+                                    <Form.Group controlId="formBasicPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control type="password" placeholder="Password" />
+                                    </Form.Group>
+                                    <Form.Group controlId="formBasicCheckbox">
+                                        <Form.Check type="checkbox" label="Check me out" />
+                                    </Form.Group>
+                                    <Row>
+                                        <Col><Button variant="primary" type="submit">
+                                            Login
+                                        </Button></Col>
+                                        <Col> <Button as={Link} to="/" variant="danger" type="submit">
+                                            Back
+                                        </Button></Col>
+                                        <Col></Col>
+                                    </Row>
+
+                                </Form>
+                            </Card.Body>
+                        </Card></Col>
+                        <Col></Col>
+                    </Row>
+
                 </Container>
             </div>
         );
