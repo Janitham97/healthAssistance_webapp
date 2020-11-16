50e26e2b26dc03542af94973b5622533216bb7d6
Janitha Missaka <janitha.missaka@gmail.com>
2020-11-16 20:07:08 +0530

    install react hook form

--- src/main/FrontEnd/reactjs/src/Components/SignupFourmDoc.js
+++ src/main/FrontEnd/reactjs/src/Components/SignupFourmDoc.js
@@ -3,6 +3,7 @@ import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
 class SignupFourmDoc extends Component{
+
     render() {
         return (
             <div>
@@ -14,40 +15,40 @@ class SignupFourmDoc extends Component{
                         <Col></Col>
                         <Col><Card style={{ width: '30rem' }}>
                             <Card.Body>
-                                <Form>
+                                <Form onSubmit={this.submitDoc} id="DocRegForm">
                                     <Form.Row>
-                                        <Form.Group as={Col} controlId="formGridEmail">
+                                        <Form.Group as={Col} controlId="formfirstname">
                                             <Form.Label>First Name</Form.Label>
-                                            <Form.Control type="email" placeholder="Enter First Name" />
+                                            <Form.Control required type="test" name="fisrtname" value={this.state.firstname} onChange={this.docChange} placeholder="Enter First Name" />
                                         </Form.Group>
 
-                                        <Form.Group as={Col} controlId="formGridPassword">
+                                        <Form.Group as={Col} controlId="formlastname">
                                             <Form.Label>Last Name</Form.Label>
-                                            <Form.Control type="password" placeholder="Enter Last Name" />
+                                            <Form.Control required type="text" name="lastname" placeholder="Enter Last Name" />
                                         </Form.Group>
                                     </Form.Row>
                                     <Form.Row>
-                                        <Form.Group as={Col} controlId="formGridEmail">
+                                        <Form.Group as={Col} controlId="formnic">
                                             <Form.Label>NIC</Form.Label>
-                                            <Form.Control type="email" placeholder="Enter Your NIC" />
+                                            <Form.Control required type="test" name="nic" placeholder="Enter Your NIC" />
                                         </Form.Group>
                                     </Form.Row>
                                     <Form.Row>
-                                        <Form.Group as={Col} controlId="formGridEmail">
+                                        <Form.Group as={Col} controlId="formlocation">
                                             <Form.Label>Location</Form.Label>
-                                            <Form.Control type="email" placeholder="Enter Your Near City" />
+                                            <Form.Control required type="test"  name="location" placeholder="Enter Your Near City" />
                                         </Form.Group>
                                     </Form.Row>
                                     <Form.Row>
-                                        <Form.Group as={Col} controlId="formGridEmail">
+                                        <Form.Group as={Col} controlId="formpassword">
                                             <Form.Label>Password</Form.Label>
-                                            <Form.Control type="email" placeholder="Enter New Password" />
+                                            <Form.Control required type="password" name="password" placeholder="Enter New Password" />
                                         </Form.Group>
                                     </Form.Row>
                                     <Form.Row>
-                                        <Form.Group as={Col} controlId="formGridEmail">
+                                        <Form.Group as={Col} controlId="formconpassword">
                                             <Form.Label>Confirm Password</Form.Label>
-                                            <Form.Control type="text" placeholder="Confirm Your Password" />
+                                            <Form.Control required type="password" name="conpassword" placeholder="Confirm Your Password" />
                                         </Form.Group>
                                     </Form.Row>
 
