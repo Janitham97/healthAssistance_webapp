515a597375b5aeaeab9444404acfc84bd8f50e59
Janitha Missaka <janitha.missaka@gmail.com>
2020-11-24 15:49:16 +0530

    add nic and password validations

--- src/main/FrontEnd/reactjs/src/Components/SignupFourmDoc.js
+++ src/main/FrontEnd/reactjs/src/Components/SignupFourmDoc.js
@@ -4,6 +4,41 @@ import {Link} from "react-router-dom";
 
 
 class SignupFourmDoc extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstname: '',
+            lastname: '',
+            nic:'',
+            password:'',
+            conpassword:''
+        };
+    }
+
+    myChangeHandler = (event) => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
+    }
+
+    mySubmitHandler = (event) =>{
+
+        let nic = this.state.nic;
+        let pass = this.state.password;
+        let pass1 = this.state.conpassword;
+        let valnic =RegExp(/^([0-9]{9}[x|X|v|V]|[0-9]{12})$/)
+
+        if (nic != valnic){
+            alert("NIC not formatted")
+        }
+
+        if (pass != pass1) {
+            alert("password should be match");
+        }
+    }
+
+
+
 
     render() {
         return (
@@ -16,7 +51,7 @@ class SignupFourmDoc extends Component{
                         <Col></Col>
                         <Col><Card style={{ width: '30rem' }}>
                             <Card.Body>
-                                <Form onSubmit={this.onSubmit} id="DocRegForm">
+                                <Form onSubmit={this.mySubmitHandler} id="DocRegForm">
                                     <Form.Row>
                                         <Form.Group as={Col} controlId="formfirstname">
                                             <Form.Label>First Name</Form.Label>
@@ -25,37 +60,57 @@ class SignupFourmDoc extends Component{
                                                 type="test"
                                                 name="firstname"
                                                 placeholder="Enter First Name"
-                                                onChange={this.changeHandler}
+                                                onChange={this.myChangeHandler}
                                             />
                                         </Form.Group>
 
                                         <Form.Group as={Col} controlId="formlastname">
                                             <Form.Label>Last Name</Form.Label>
-                                            <Form.Control required type="text" name="lastname" placeholder="Enter Last Name" />
+                                            <Form.Control required
+                                                          type="text"
+                                                          name="lastname"
+                                                          placeholder="Enter Last Name"
+                                                          onChange={this.myChangeHandler}/>
                                         </Form.Group>
                                     </Form.Row>
                                     <Form.Row>
                                         <Form.Group as={Col} controlId="formnic">
                                             <Form.Label>NIC</Form.Label>
-                                            <Form.Control required type="test" name="nic" placeholder="Enter Your NIC" />
+                                            <Form.Control required
+                                                          type="test"
+                                                          name="nic"
+                                                          placeholder="Enter Your NIC"
+                                                          onChange={this.myChangeHandler}/>
                                         </Form.Group>
                                     </Form.Row>
                                     <Form.Row>
                                         <Form.Group as={Col} controlId="formlocation">
                                             <Form.Label>Location</Form.Label>
-                                            <Form.Control required type="test"  name="location" placeholder="Enter Your Near City" />
+                                            <Form.Control required
+                                                          type="test"
+                                                          name="location"
+                                                          placeholder="Enter Your Near City"
+                                                          onChange={this.myChangeHandler}/>
                                         </Form.Group>
                                     </Form.Row>
                                     <Form.Row>
                                         <Form.Group as={Col} controlId="formpassword">
                                             <Form.Label>Password</Form.Label>
-                                            <Form.Control required type="password" name="password" placeholder="Enter New Password" />
+                                            <Form.Control required
+                                                          type="password"
+                                                          name="password"
+                                                          placeholder="Enter New Password"
+                                                          onChange={this.myChangeHandler}/>
                                         </Form.Group>
                                     </Form.Row>
                                     <Form.Row>
                                         <Form.Group as={Col} controlId="formconpassword">
                                             <Form.Label>Confirm Password</Form.Label>
-                                            <Form.Control required type="password" name="conpassword" placeholder="Confirm Your Password" />
+                                            <Form.Control required
+                                                          type="password"
+                                                          name="conpassword"
+                                                          placeholder="Confirm Your Password"
+                                                          onChange={this.myChangeHandler}/>
                                         </Form.Group>
                                     </Form.Row>
 
